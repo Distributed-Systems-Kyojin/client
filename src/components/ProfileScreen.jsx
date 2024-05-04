99ace40f9a7ae46a108f776db6802d848473b397
akashtharuka <akash_tharuka@yahoo.com>
2024-05-04 18:40:36 +0530

    Home page layout

---
+++ src/components/ProfileScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from '@material-tailwind/react';
+
+const ProfileScreen = () => {
+  return (
+    <Card className="h-[calc(100vh-2rem)] w-full p-8 shadow-xl shadow-blue-gray-900/5">
+        Profile
+    </Card>
+  );
+}
+
+export default ProfileScreen;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Sidebar from '../components/Sidebar';
+import ListFilesScreen from '../components/ListFilesScreen';
+import UploadFilesScreen from '../components/UploadFilesScreen';
+import Dashboard from '../components/Dashboard';
+import ProfileScreen from '../components/ProfileScreen';
 
 const Home = () => {
+
+  const [selectedScreen, setSelectedScreen] = useState('dashboard');
+
   return (
-    <div>Home</div>
+    <div className='bg-background fixed flex w-full h-full'>
+      <div className="container mx-auto flex">
+        <div className="sidebar container flex-1 content-center p-4">
+          <Sidebar setSelectedScreen={setSelectedScreen} />
+        </div>
+        <div className="content container flex-auto content-center p-4 pl-0">
+          {selectedScreen === 'dashboard' && <Dashboard />}
+          {selectedScreen === 'listfiles' && <ListFilesScreen />}
+          {selectedScreen === 'searchfiles' && <ListFilesScreen />}
+          {selectedScreen === 'uploadfiles' && <UploadFilesScreen />}
+          {selectedScreen === 'profile' && <ProfileScreen />}
+        </div>
+      </div>
+    </div>
   );
 }
 
