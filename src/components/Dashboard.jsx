9e73d9701aa576111a11e0c859079b46cb44601f
akashtharuka <akash_tharuka@yahoo.com>
2024-05-04 18:36:50 +0530

    Dashboard screen ui component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from '@material-tailwind/react';
+
+const Dashboard = () => {
+  return (
+    <Card className="h-[calc(100vh-2rem)] w-full p-8 shadow-xl shadow-blue-gray-900/5">
+        Dashboard
+    </Card>
+  );
+}
+
+export default Dashboard;
