6e6d15d56d184fb600219a7067406d46afc137e9
akashtharuka <akash_tharuka@yahoo.com>
2024-06-06 22:59:18 +0530

    added error handling to useFileList hook

--- src/hooks/useFileList.js
+++ src/hooks/useFileList.js
@@ -1,11 +1,26 @@
 import { useCallback } from "react";
-import api from "../services/api";
+import useAxiosPrivate from "./useAxiosPrivate";
+import useAuth from "./useAuth";
+
+const GET_FILE_LIST_URL = "/file/getFileList";
 
 const useFileList = () => {
+
+    const { auth } = useAuth();
+    const axiosPrivate = useAxiosPrivate();
+
     const fetchFileList = useCallback(async () => {
-        const response = await api.get("/file/getFileList");
-        return response.data;
-    }, []);
+        try {
+            const response = await axiosPrivate.get(GET_FILE_LIST_URL);
+            return response.data;
+        } catch (error) {
+            if (error?.response?.data?.error.message === "jwt expired") {
+                console.log("jwt expired");
+            } else {
+                throw error;
+            }
+        }
+    }, [auth]);
 
     return { fetchFileList };
 }
